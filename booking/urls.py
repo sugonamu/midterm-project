aacfde1299c5c83389c31107acbf95f9a092d2e2
Brian <briansubekti11@gmail.com>
2024-10-26 20:52:50 +0700

    Brian push

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import booking_views
 
 app_name = 'booking'
 
 urlpatterns = [
-    path('search/', views.hotel_search, name='hotel_search'),
-    path('book/<int:hotel_id>/', views.book_hotel, name='book_hotel'),
-    path('booking/success/', views.booking_success, name='booking_success'),  # Success page after booking
+    path('',booking_views.hotel_search,name='hotel_search'),
+    path('book/<int:hotel_id>/', booking_views.book_hotel, name='book_hotel'),
+    path('booking/success/', booking_views.booking_success, name='booking_success'),  # Success page after booking
 ]
