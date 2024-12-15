0599546bd9a05db63ecde690afc50ca727f8201b
Brian <briansubekti11@gmail.com>
2024-12-15 22:06:24 +0700

    Booking

--- booking/urls.py
+++ booking/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('book/<int:hotel_id>/', booking_views.book_hotel, name='book_hotel'),
     path('booking/success/', booking_views.booking_success, name='booking_success'),  # Success page after booking
     path('hotel/<int:hotel_id>/add-rating/', booking_views.add_rating, name='add_rating'),
-        path('ratings/json/', booking_views.show_json, name='show_json')
+    path('ratings/json/', booking_views.show_json, name='show_json'),
+    path('api/hotels/', booking_views.HotelList.as_view(), name='hotel-list'),
+    path('api/hotels/<int:pk>/', booking_views.HotelDetail.as_view(), name='hotel-detail'),
+    path('api/hotels/<int:hotel_id>/ratings/', booking_views.HotelRatings.as_view(), name='hotel-ratings'),
+    path('api/hotels/<int:hotel_id>/add-rating/', booking_views.AddRating.as_view(), name='add-rating'),
 ]
+
