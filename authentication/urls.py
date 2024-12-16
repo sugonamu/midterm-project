47291b4f3882fbc4610a491f63002a1150a7d392
sugonamu <je0jwff@gmail.com>
2024-12-16 22:00:05 +0700

    edit n delete function

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login, register, logout
+from .views import login, register, logout, edit_property, delete_property
 
 app_name = 'authentication'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout, name='logout'),
+    path('editproperty/<uuid:property_id>/', edit_property, name='edit_property'),
+    path('delete/<uuid:property_id>/', delete_property, name='delete_property'),
 ]
